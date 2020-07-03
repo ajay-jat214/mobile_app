fb5524a0c1c7d9296310da8b370be828d3703fb0
Ajay Jat <jatajay004@gmail.com>
2020-07-03 23:35:44 +0530

    eotfw

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ app.post('/account/signin',(req,res)=>{
 })
 
 if(process.env.NODE_ENV==="production"){
-   app.use(express.static('shopping/build'),{maxage:31536000});
+   app.use(express.static('shopping/build'));
    const path=require('path');
    app.get("*",(req,res)=>{
      res.sendFile(path.resolve(__dirname,'shopping','build','index.html'));
